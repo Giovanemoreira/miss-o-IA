3db0508c04c25d6dc3e0ca83299115c194cf877f
Giovanemoreira <giovane.demarque@escola.pr.gov.br>
2026-08-03 08:22:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+{
+    enunciado: "Ao sair da escola, Davi vê um parque cheio de lixo e pensa no inpacto disso para a natureza. Oque ele decide?",
+    alternativas: [
+        "Cuida do meio anbiente é responsabilidade de todos.",
+        "Pequenas atitudes não fazem, diferença para o meio anbiente"
+    ]
+},
+{
+    enunciado: "Depois de aprender sobre a preservação de natureza a professora pode que Davi proponha uma ação para reduzir o lixo na escola. Qual atituide Davi toma?",
+    alternativas: [
+        "Orgonizar campanha de reciclagem com a turma",
+        "Decide não participar da campanha porque acredita que fará diferença"
+    ]
+},
+{
+    enunciado: "Pe1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+},
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+}
+];
