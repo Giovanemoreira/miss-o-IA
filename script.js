6a85f2249713523b296883a6cd955868fa15e9f4
Giovanemoreira <giovane.demarque@escola.pr.gov.br>
2026-08-10 08:25:34 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,29 +8,58 @@ const perguntas = [
 {
     enunciado: "Ao sair da escola, Davi vê um parque cheio de lixo e pensa no impacto disso para a natureza. Oque ele decide?",
     alternativas: [
-        "Cuida do meio anbiente é responsabilidade de todos.",
-        "Pequenas atitudes não fazem, diferença para o meio anbiente."
+        {
+            texto: "Cuida do meio anbiente é responsabilidade de todos.",
+         afirmacao:""
+        },
+        {
+            texto:"Pequenas atitudes não fazem, diferença para o meio anbiente."
+            afirmacao:""
+        }
+        
     ]
 },
 {
     enunciado: "Depois de aprender sobre a preservação de natureza a professora pode que Davi proponha uma ação para reduzir o lixo na escola. Qual atituide Davi toma?",
     alternativas: [
-        "Orgonizar campanha de reciclagem com a turma.",
-        "Decide não participar da campanha porque acredita que fará diferença."
+        {
+            texto:"Pequenas atitudes não fazem, diferença para o meio anbiente."
+            afirmacao:
+        },
+        {
+            texto:"Pequenas atitudes não fazem, diferença para o meio anbiente."
+            afirmacao:
+        },
     ]
 },
 {
     enunciado: "Depois de Davi participar de um projeto sobre preservação da natureza a turma iniciou uma discussão sobre como o proteger o meio anbiente. Oque Davi fez?",
     alternativas: [
-        "Defede a reciclagem, a economia de água e o plantio de árvore.",
-        "Afirma que pequenas açoẽs não fazem a diferença e que a responsabilidade é apenas do governo."
+        {
+           texto:"Defede a reciclagem, a economia de água e o plantio de árvore.",
+           afirmacao:
+        }
+        {
+           texto:"Afirma que pequenas açoẽs não fazem a diferença e que a responsabilidade é apenas do governo."
+           afirmacao:
+        }
+        
+        
     ]
 },
 {
     enunciado: "Ao final da discussão, Davi precisou criar um cartaz no computador para concientizar as pessoas sobre a preservação do meio anbiente. E agora?",
     alternativas: [
-        "Criar um cartaz com imagens sobre reciclagem e preservação da natureza.",
-        "Criar um cartaz dizendo que as açoẽs das pessoas influenciam na preserevação da natureza."
+        {
+            texto:"Criar um cartaz com imagens sobre reciclagem e preservação da natureza.",
+            afirmacao:
+        }
+        {
+            texto:"Criar um cartaz dizendo que as açoẽs das pessoas influenciam na preserevação da natureza."
+            afirmacao:
+        }
+        
+        
     ]
 },
 {
@@ -50,4 +79,4 @@ function mostraPergunta() {
     caixaPerguntas.textContent = perguntaAtual.enunciado;
 }
 
-mostraPergunta()
+mostraPergunta() 
