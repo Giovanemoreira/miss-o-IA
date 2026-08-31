96c1f9552bb268f53e46d63f16822235537ef95c
Giovanemoreira <giovane.demarque@escola.pr.gov.br>
2026-08-31 07:39:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,77 +6,72 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
 {
-    enunciado: "Ao sair da escola, Davi vê um parque cheio de lixo e pensa no impacto disso para a natureza. Oque ele decide?",
+    enunciado: "Ao sair da escola, Davi vê um parque cheio de lixo e pensa no impacto disso para a natureza. O que ele decide?",
     alternativas: [
         {
-            texto: "Cuida do meio anbiente é responsabilidade de todos.",
-         afirmacao:"Cuidar do meio ambiente é responsabilidade de todos e pequenas atitudes ajudam a preserva a natureza."
+            texto: "Cuidar do meio ambiente é responsabilidade de todos.",
+            afirmacao: "Cuidar do meio ambiente é responsabilidade de todos e pequenas atitudes ajudam a preservar a natureza."
         },
         {
-            texto:"Pequenas atitudes não fazem, diferença para o meio anbiente.",
-            afirmacao:"a preservação do meio ambiente depende apenas do governo, por isso as ações individuais não fazem defirença."
-        }
-        
+            texto: "Pequenas atitudes não fazem, diferença para o meio ambiente.",
+            afirmacao: "A preservação do meio ambiente depende apenas do governo, por isso as ações individuais não fazem diferença."
+        }  
     ]
 },
 {
-    enunciado: "Depois de aprender sobre a preservação de natureza a professora pode que Davi proponha uma ação para reduzir o lixo na escola. Qual atituide Davi toma?",
+    enunciado: "Depois de aprender sobre a preservação da natureza, a professora pede que Davi proponha uma ação para reduzir o lixo na escola. Qual atitude Davi toma?",
     alternativas: [
         {
-            texto:"Pequenas atitudes não fazem, diferença para o meio anbiente.",
-            afirmacao:"Pequenas ações coletivaS ajudam apreserva o meio ambiente."
+            texto: "Organiza uma campanha de reciclagem com a turma.",
+            afirmacao: "Pequenas ações coletivas ajudam a preservar o meio ambiente."
         },
         {
-            texto:"Pequenas atitudes não fazem, diferença para o meio anbiente.",
-            afirmacao:"a participação das pessoas não é essencial para reduzir os inpactos ambientais"
-        }
+            texto: "Decide não participar da campanha porque acredita que não fará diferença.",
+            afirmacao: "A participação das pessosas não é essencial para reduzir os impactos ambientais."
+        }    
     ]
 },
 {
-    enunciado: "Depois de Davi participar de um projeto sobre preservação da natureza a turma iniciou uma discussão sobre como o proteger o meio anbiente. Oque Davi fez?",
+    enunciado: "Depois de Davi participou de um projeto sobre preservação da natureza a turma iniciou um discussão sobre como proteger o meio ambiente. O que Davi faz?",
     alternativas: [
         {
-           texto:"Defede a reciclagem, a economia de água e o plantio de árvore.",
-           afirmacao:"As atitudes sustentáveis contribuem para a preservação do anbiente."
+            texto: "Defende a reciclagem, a economia de água e o plantio de árvores.",
+            afirmacao: "As atitudes sustentáveis contribuem para preservação do ambiente."
         },
         {
-           texto:"Afirma que pequenas açoẽs não fazem a diferença e que a responsabilidade é apenas do governo.",
-           afirmacao:"A participação da sociedade não é de importância para proteger o meio anbiente"
-        }
-        
-        
+            texto: "Afirma que pequenas ações não fazem diferença e que a responsabilidade é apenas do governo.",
+            afirmacao: "A participação da sociedade não é de importância para proteger o meio ambiente."
+        }    
     ]
 },
 {
-    enunciado: "Ao final da discussão, Davi precisou criar um cartaz no computador para concientizar as pessoas sobre a preservação do meio anbiente. E agora?",
+    enunciado: "Ao final da discussão, Davi precisou criar um cartaz no computador para conscientizar as pessoas sobre a preservação do meio ambiente. E agora?",
     alternativas: [
         {
-            texto:"Criar um cartaz com imagens sobre reciclagem e preservação da natureza.",
-            afirmacao:"A concientização ambiental incentiva atitudes sutentáveis e juda a preserva o meio ambiente."
+            texto: "Criar um cartaz com imagens e mensagens sobre reciclagem e preservação da natureza.",
+            afirmacao: "A conscientização ambiental incentiva atitudes sustentáveis e ajuda a preservar o meio ambiente."
         },
         {
-            texto:"Criar um cartaz dizendo que as açoẽs das pessoas influenciam na preserevação da natureza.",
-            afirmacao:"As ações individuais não são importantes para proteger a natureza."
-        }
-        
-        
+            texto:"Criar um cartaz dizendo que as ações das pessoas não influenciam na preservação da natureza.",
+            afirmacao:"As ações individuais e coletivas não são importantes para proteger a natureza."
+        }      
     ]
 },
 {
-    enunciado: "Depois de apresentar o cartaz, a professora propõs uma ação para reduzir o desperdicio na escola. Oque Davi fez?",
+    enunciado: "Depois de apresentar o cartaz, a professora propôs uma ação para reduzir o desperdicio na escola. O que Davi faz?",
     alternativas: [
         {
             texto:"Organiza uma campanha para economizar água e separar o lixo reciclável.",
-            afirmacao:"Economizar recursos naturais e reciclar contribuem para a preservação da natureza."
+            afirmacao: "Economizar recursos naturais e reciclar contribuem para a preservação da natureza."
         },
-        {      
-            texto: "Comtinua despediçando água e jogando lixo em qualquer lugar.",
-            afirmacao:"o desperdício de recursos e o descarte incorreto de resíduos não prejudica o meio ambiente."
-        }
-       
+        {
+            texto: "Continua desperdiçando água e jogando lixo em qualquer lugar.",
+            afirmacao: "O desperdício de recursos e o descarte incorreto de resíduos não prejudica o meio ambiente."
+        }  
     ]
-}
+ },
 ];
+
 
 let atual = 0;
 let perguntaAtual;
@@ -103,8 +98,8 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacoes;
-    historiaFinal += afirmacoes + " ";
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacao + "  ";
     atual++;
     mostraPergunta();
 }
